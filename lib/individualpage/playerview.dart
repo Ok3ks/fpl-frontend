4f615fb78a1c49f15cee5619cb37b2b1204d9f57
Emmanuel Okedele <emmanuel@coefficient.ai>
2024-08-22 20:30:15 +0100

    Frontend scaffold

---
+++ lib/individualpage/playerview.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PlayerView extends StatelessWidget {
+  const PlayerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
