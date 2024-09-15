c2ceddbcf2cff316227f08c460a30e09da5da1ba
Emmanuel Okedele <emmanuel@coefficient.ai>
2024-09-15 23:40:16 +0100

    Ngrok backend

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 abstract class Constants {
-  static const String devUrl = 'http://localhost:8000/graphql/';
+  // static const String devUrl = 'http://localhost:8000/graphql/';
+  static const String devUrl = 'https://lamprey-finer-partially.ngrok-free.app/graphql/';
   // static const String prodUrl = 'https://robin.coefficient.tools/api/graphql/';
 }
