6755d158006e2ce27bf629fa6ba5808f4cdf649a
Emmanuel Okedele <emmanuel@coefficient.ai>
2024-10-13 15:12:02 +0100

    version 13-10-2024

--- lib/dataprovider.dart
+++ lib/dataprovider.dart
@@ -83,5 +83,5 @@ final leagueProvider = StateProvider<double?>((ref) {
 });
 
 final gameweekProvider = StateProvider<double>((ref) {
-  return 1;
+  return 7; //Should start from current gameweek
 });
