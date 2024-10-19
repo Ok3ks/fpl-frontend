a5789a102ad4e3ede77a5c7f688391b9764ddef3
Emmanuel Okedele <emmanuel@coefficient.ai>
2024-10-19 15:38:29 +0100

    devUrl to prodUrl

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,10 @@
 abstract class Constants {
   // static const String devUrl = 'http://127.0.0.1:8000/graphql/';
-  static const String prodUrl = 'https://fpl-backend-1054719499081.europe-west2.run.app/graphql/';
+  static const String prodUrl =
+      'https://fpl-backend-1054719499081.europe-west2.run.app/graphql/';
   // static const String devUrl = 'https:backend.dontsuckatfpl.app/graphql/';
   // static const String devUrl = 'https://lamprey-finer-partially.ngrok-free.app/graphql/';
-  static const String fplUrl = "https://fantasy.premierleague.com/api/bootstrap-static/";
+  static const String fplUrl =
+      "https://fantasy.premierleague.com/api/bootstrap-static/";
 // static const String prodUrl = 'https://robin.coefficient.tools/api/graphql/';
 }
