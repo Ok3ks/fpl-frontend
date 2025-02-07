a4462538132ce3ecf5529ee34c7f877f8fe5898a
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-02-07 10:46:31 +0000

    Rename to onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpl/onboarding/onboarding.dart';
 import 'package:fpl/themes.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:go_router/go_router.dart';
@@ -69,7 +70,7 @@ final GoRouter router = GoRouter(
     GoRoute(
     path: '/landing',
     builder: (BuildContext context, GoRouterState state) {
-      return const MyApp();
+      return ProviderScope(child: Onboarding());
     }),
   ],
   routerNeglect: true,
