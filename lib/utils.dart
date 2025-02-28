3dc0a1691880d14f20a9973fcb3267bea6b662b1
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-02-28 23:17:44 +0000

    dart format

--- lib/utils.dart
+++ lib/utils.dart
@@ -236,7 +236,8 @@ class playerName extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return FutureBuilder(
-        future: pullPlayerStats(playerId, gameweek ?? ref.watch(gameweekProvider)),
+        future:
+            pullPlayerStats(playerId, gameweek ?? ref.watch(gameweekProvider)),
         builder: (context, snapshot) {
           var obj = snapshot.data;
           if (snapshot.hasData) {
