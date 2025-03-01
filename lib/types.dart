caa47a034b7dc38f6642f9c5ad6cd94ebbf9365b
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-03-01 00:29:43 +0000

    User type

---
+++ lib/types.dart
@@ -0,0 +1,61 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class User {
+  final String email;
+  String? username;
+  String? favoriteTeam;
+  String? fplUrl;
+  String? yearsPlayingFpl;
+  String? location;
+  String? password;
+  String? error;
+
+  User(
+      {required this.email,
+      this.favoriteTeam,
+      this.username,
+      this.fplUrl,
+      this.yearsPlayingFpl,
+      this.location,
+      this.password});
+
+  Future<UserCredential?> registerUser() async {
+    try {
+      UserCredential firebaseUser = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: email, password: password ?? "VRBWX6k3gZ");
+      print(email);
+      print(password);
+      //TODO: add additional info to firebaseUser with fireStore
+      print(firebaseUser);
+      if (firebaseUser.user != null) {
+        await firebaseUser.user?.sendEmailVerification();
+      }
+      return firebaseUser;
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+      if (e.code == 'weak-password') {
+        error = 'The password provided is too weak';
+        return null;
+      } else if (e.code == 'email-already-in-use') {
+        error = 'The account already exists for that email.';
+        return null;
+      }
+      return null;
+    }
+  }
+
+  Future<UserCredential?> retrieveUser(String password) async {
+    try {
+      UserCredential loggedInFirebaseUser = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: email, password: password);
+      return loggedInFirebaseUser;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+
+
+  }
+}
