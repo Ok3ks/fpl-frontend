4954f130843916bdb44e5166d133a10e476820cd
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-03-18 11:15:33 +0000

    cleanup: remove unnecessary imports

--- lib/leaguepage/captainmetrics.dart
+++ lib/leaguepage/captainmetrics.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpl/dataprovider.dart';
 import 'package:fpl/themes.dart';
