10fd0a8c81b5367d908f8a3321a21d289ea7fc9d
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-03-24 14:05:08 +0000

    Modularise leagueName, and hydrate flag in preparation for optimistic u.i of other elements

---
+++ lib/leaguepage/leagueName.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:fpl/themes.dart';
+
+class leagueName extends StatelessWidget {
+  String? name;
+  bool hydrate;
+
+  leagueName({super.key, required this.name, this.hydrate = false});
+
+  @override
+  Widget build(BuildContext context) {
+    // String  name = data['leagueWeeklyReport']['leagueName'];
+    if (hydrate == false) {
+      return Text("$name ",
+          style: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+            decoration: TextDecoration.none,
+          ));
+    } else {
+      //typing animation
+      return const Text("typing..",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+            decoration: TextDecoration.none,
+          ));
+    }
+  }
+}
+
+
