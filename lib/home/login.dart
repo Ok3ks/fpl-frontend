7fc789e600c93346bc4751081cd07c40c45f2b77
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-03-31 22:52:01 +0100

    simplify login, and add forgot password

--- lib/home/login.dart
+++ lib/home/login.dart
@@ -36,11 +36,20 @@ class _LoginBoxState extends ConsumerState<LoginBox> {
   final TextEditingController _passwordController = TextEditingController();
   String _errorMessage = '';
   bool toggled = true;
+  bool signIn = false;
+  bool signInWithPassWord = false;
+  bool forgotPassword = false;
 
   void toggleObscurePassword() {
     setState(() {
       toggled = !toggled;
     });
+  }
+
+  void _resetPassword() async {
+    Participant currentUser = Participant(
+        email: _emailController.text, password: _passwordController.text);
+    await currentUser.sendEmailLink();
   }
 
   void _login() async {
@@ -112,61 +121,121 @@ class _LoginBoxState extends ConsumerState<LoginBox> {
     final currentUser = ref.watch(currentUserProvider);
     if (currentUser == null) {
       return Scaffold(
-        appBar: AppBar(
-          title: const Text('Login'),
-        ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Card(
-              elevation: 4,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                      ),
-                    ),
-                    TextField(
-                      controller: _passwordController,
-                      obscureText: toggled ? true : false,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        errorText:
-                            _errorMessage.isNotEmpty ? _errorMessage : null,
-                      ),
-                    ),
-                    IconButton(
-                        onPressed: toggleObscurePassword,
-                        icon: const Icon(Icons.remove_red_eye)),
-                    const SizedBox(height: 20),
-                    // SizedBox(height: 20, child : Text("This ")),
-                    ElevatedButton(
-                      onPressed: _login,
-                      child: const Text('Sign In'),
-                    ),
-                    TextButton(
-                      onPressed: _register,
-                      child: const Text('Register'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        // Logic for sending email link for password reset
-                        print('Send password reset email');
-                      },
-                      child: const Text('Forgot Password?'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+          appBar: AppBar(
+            title: const Text('Login'),
           ),
-        ),
-      );
+          body: Center(
+              child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: !forgotPassword
+                      ? Card(
+                          elevation: 4,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  TextField(
+                                    controller: _emailController,
+                                    decoration: InputDecoration(
+                                      labelText: 'Email',
+                                    ),
+                                  ),
+                                  if (signInWithPassWord)
+                                    // Row(
+                                    //   children: [
+                                    TextField(
+                                      controller: _passwordController,
+                                      obscureText: toggled ? true : false,
+                                      decoration: InputDecoration(
+                                        labelText: 'Password',
+                                        errorText: _errorMessage.isNotEmpty
+                                            ? _errorMessage
+                                            : null,
+                                      ),
+                                    ),
+                                  if (signInWithPassWord)
+                                    IconButton(
+                                        onPressed: toggleObscurePassword,
+                                        icon: const Icon(Icons.remove_red_eye)),
+                                  // ]),
+
+                                  const SizedBox(height: 20),
+                                  Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        ElevatedButton(
+                                            onPressed: () {
+                                              if (_passwordController
+                                                  .text.isEmpty) {
+                                                setState(() {
+                                                  signInWithPassWord = true;
+                                                });
+                                              } else {
+                                                _login();
+                                              }
+                                            },
+                                            child: const Text(
+                                              'Sign In',
+                                            ),
+                                            style: signInWithPassWord
+                                                ? ButtonStyle()
+                                                : ButtonStyle()),
+                                        const SizedBox(width: 20),
+                                        if (signInWithPassWord)
+                                          ElevatedButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                forgotPassword = true;
+                                              });
+                                              // _resetPassword();
+                                            },
+                                            child:
+                                                const Text('Forgot Password'),
+                                          ),
+                                      ]),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: _register,
+                                    child: const Text('Register'),
+                                  ),
+                                ]),
+                          ),
+                        )
+                      : Card(
+                          elevation: 4,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: SizedBox(
+                              height: 60,
+                              child: Column(children: [
+                                Text(
+                                    "A link to reset your password has been sent. Check your inbox"),
+                                Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      IconButton(
+                                        icon: const Icon(Icons.mail),
+                                        onPressed: () {
+                                          // context.go("/");
+                                        },
+                                      ),
+                                      IconButton(
+                                        icon: const Icon(Icons.keyboard_return),
+                                        onPressed: () {
+                                          setState(() {
+                                            forgotPassword = false;
+                                          });
+                                          // context.go("/login");
+                                        },
+                                      )
+                                    ])
+                              ]),
+                            ),
+                          )))));
     } else {
       //TODO: Persist Login Session
       return const Home();
