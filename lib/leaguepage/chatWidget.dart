39d194af543957a2e3d52ea1b2d074ac68d0a8e2
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-07-26 12:05:11 +0100

    Send messages to firebase

--- lib/leaguepage/chatWidget.dart
+++ lib/leaguepage/chatWidget.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpl/dataprovider.dart';
-import 'package:fpl/leaguepage/differential.dart';
+import 'package:uuid/uuid.dart';
 import 'package:fpl/themes.dart';
-import 'package:fpl/utils.dart';
-import 'package:fpl/leaguepage/benchmetrics.dart';
-import 'package:fpl/leaguepage/captainmetrics.dart';
-import 'package:fpl/leaguepage/transfermetrics.dart';
-import 'package:fpl/leaguepage/performancemetrics.dart';
-import 'package:fpl/leaguepage/leagueName.dart';
 import 'package:fpl/types.dart';
 import 'dart:convert';
 
@@ -30,6 +24,7 @@ class ChatState extends ConsumerState<Chat> {
   Widget build(BuildContext context) {
     final leagueId = ref.watch(leagueProvider)?.leagueId;
     final gameweek = ref.watch(gameweekProvider);
+    final currentUser = ref.watch(currentUserProvider);
 
     if (leagueId != null) {
       return Column(children: [
@@ -39,10 +34,21 @@ class ChatState extends ConsumerState<Chat> {
               var obj = snapshot.data;
               print(snapshot.connectionState);
               if (snapshot.hasData) {
-                return chatWidget(data: obj, width: widget.chatBoxWidth);
+                return chatWidget(
+                    data: obj,
+                    width: widget.chatBoxWidth,
+                    gameweek: gameweek,
+                    leagueId: leagueId,
+                    user: currentUser);
               } else if (snapshot.connectionState == ConnectionState.waiting) {
                 return chatWidget(
-                    data: obj, hydrate: false, width: widget.chatBoxWidth);
+                  data: obj,
+                  hydrate: false,
+                  width: widget.chatBoxWidth,
+                  gameweek: gameweek,
+                  leagueId: leagueId,
+                  user: currentUser,
+                );
               } else {
                 return const Text("No Data");
               }
@@ -57,87 +63,101 @@ class chatWidget extends StatelessWidget {
   Map<String, dynamic>? data;
   bool hydrate = true;
   double width;
+  double gameweek;
+  double leagueId;
+  Participant? user;
 
   chatWidget(
       {super.key,
       required this.data,
       this.hydrate = true,
-      required this.width});
+      required this.width,
+      required this.gameweek,
+      required this.leagueId,
+      required this.user});
 
   TextEditingController chatController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     int msgLength = data?['msgLength'] ?? 1;
-    return Column(
+    return Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      Column(
+          children: List.generate(msgLength, (int index) {
+        return SizedBox(
+            width: width,
+            child: Card(
+                margin: const EdgeInsetsGeometry.fromLTRB(7, 10, 7, 0),
+                elevation: 8,
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(2),
+                ),
+                child: const Text(" " + "Temp",
+                    textDirection: TextDirection.rtl,
+                    softWrap: true,
+                    style: TextStyle(
+                      color: Colors.black26,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 15,
+                    ))));
+      })),
+      Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Column(
-              children: List.generate(msgLength, (int index) {
-            return SizedBox(
-                width: width,
-                child: Card(
-                    margin: const EdgeInsetsGeometry.fromLTRB(7, 10, 7, 0),
-                    elevation: 8,
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(2),
-                    ),
-                    child: const Text(" " + "Temp",
-                        textDirection: TextDirection.rtl,
-                        softWrap: true,
-                        style: TextStyle(
-                          color: Colors.black26,
-                          fontStyle: FontStyle.italic,
-                          fontSize: 15,
-                        ))));
-          })),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                  SizedBox(
-                  width: width - 60,
-                  child: TextField(
-                    style: const TextStyle(fontSize: 9, color: Colors.white),
-                    cursorColor: MaterialTheme.darkMediumContrastScheme().primary,
-                    controller: chatController,
-                    // textInputAction: TextInputAction.done,
-                    decoration: InputDecoration(
-                        hintText: 'Send a message',
-                        hintStyle: const TextStyle(
-                            color: Colors.grey,
-                            fontSize: 10,
-                            fontWeight: FontWeight.w100,
-                            fontStyle: FontStyle.italic),
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                                color: MaterialTheme.darkMediumContrastScheme()
-                                    .primaryContainer)),
-                        disabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                                color: MaterialTheme.darkMediumContrastScheme()
-                                    .primaryContainer)),
-                        enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                                color: MaterialTheme.darkMediumContrastScheme()
-                                    .primaryContainer)),
-                        border: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                                color: MaterialTheme.darkMediumContrastScheme()
-                                    .primaryContainer)),
-                        contentPadding: const EdgeInsets.symmetric(horizontal: 8),
-                        fillColor: Colors.white,
-                        iconColor: Colors.white),
-                    cursorHeight: 20,
-                    autocorrect: false,
-                  )),
-                  IconButton(
-                      onPressed: (
-                          //TODO: Send to chat History
-                          //TODO: Sync to FireStore
-                          ) async {},
-                      icon: Icon(Icons.send,
-                          color: MaterialTheme.darkMediumContrastScheme().primary))
-                  ],)]);
+          SizedBox(
+              width: width - 60,
+              child: TextField(
+                style: const TextStyle(fontSize: 9, color: Colors.white),
+                cursorColor: MaterialTheme.darkMediumContrastScheme().primary,
+                controller: chatController,
+                // textInputAction: TextInputAction.done,
+                decoration: InputDecoration(
+                    hintText: 'Send a message',
+                    hintStyle: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 10,
+                        fontWeight: FontWeight.w100,
+                        fontStyle: FontStyle.italic),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: MaterialTheme.darkMediumContrastScheme()
+                                .primaryContainer)),
+                    disabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: MaterialTheme.darkMediumContrastScheme()
+                                .primaryContainer)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: MaterialTheme.darkMediumContrastScheme()
+                                .primaryContainer)),
+                    border: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: MaterialTheme.darkMediumContrastScheme()
+                                .primaryContainer)),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+                    fillColor: Colors.white,
+                    iconColor: Colors.white),
+                cursorHeight: 20,
+                autocorrect: false,
+              )),
+          IconButton(
+              onPressed: (
+                  //TODO: Send to chat History
+                  //TODO: Sync to FireStore
+                  ) async {
+                Message message = Message(
+                    id: const Uuid().v4obj().toString(),
+                    from: user,
+                    timestamp: DateTime.now().toString(),
+                    text: chatController.text);
+                await addMessage(leagueId, gameweek, message);
+                chatController.text = "";
+              },
+              icon: Icon(Icons.send,
+                  color: MaterialTheme.darkMediumContrastScheme().primary))
+        ],
+      )
+    ]);
   }
 }
