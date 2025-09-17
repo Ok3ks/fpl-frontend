e5aa91551f554347bfb33a6785955d5f922d8a9d
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-09-17 17:52:37 +0100

    new endpoint

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,7 @@
 abstract class Constants {
-  // static const String prodUrl = 'http://0.0.0.0:8000/graphql/';
-
+  // static const String prodUrl = 'http://127.0.0.1:8000/graphql/';
   static const String prodUrl =
-      'https://league-analysis-backend-987818742353.europe-west2.run.app/graphql/';
+      'https://redeployed-backend-987818742353.europe-west9.run.app/graphql/';
   // static const String devUrl = 'https:backend.dontsuckatfpl.app/graphql/';
   // static const String devUrl = 'https://lamprey-finer-partially.ngrok-free.app/graphql/';
   static const String fplUrl =
