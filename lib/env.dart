41fffe86f4624d9ae9d4cdcd1339c898022dd74d
ok3ks <freelanceokeks@gmail.com>
2025-12-02 04:04:56 +0000

    squash! replace assets .env

---
+++ lib/env.dart
@@ -0,0 +1,10 @@
+class Env {
+  static const apiKey = String.fromEnvironment('apiKey');
+  static const authDomain = String.fromEnvironment('authDomain');
+  static const projectId = String.fromEnvironment('projectId');
+  static const storageBucket = String.fromEnvironment('storageBucket');
+  static const messagingSenderId =
+      String.fromEnvironment('messagingSenderId');
+  static const appId = String.fromEnvironment('appId');
+  static const measurementId = String.fromEnvironment('measurementId');
+}
